984b7b0301d63937036293d45527d8a85b309e00
oljoi <46825856+oljoi@users.noreply.github.com>
2025-10-31 20:52:42 +0200

    First upload

---
+++ src/app.rs
@@ -0,0 +1,69 @@
+use leptos::prelude::*;
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_router::{
+    components::{Route, Router, Routes},
+    StaticSegment,
+};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+    view! {
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <AutoReload options=options.clone() />
+                <HydrationScripts options/>
+                <MetaTags/>
+            </head>
+            <body>
+                <App/>
+            </body>
+        </html>
+    }
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/puppy-support.css"/>
+
+        // sets the document title
+        <Title text="support your puppy"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes fallback=|| "Page not found.".into_view()>
+                    <Route path=StaticSegment("") view=HomePage/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let count = RwSignal::new(1);
+    let on_click = move |_| {
+      count.update(|c| {
+         *c += 1;
+      });
+    };
+    let barks = move || "bark ".repeat(count.get().try_into().unwrap());
+
+
+    view! {
+        <h1>"woof :3"</h1>
+        <span>{barks}</span>
+        <br />
+        <button on:click=on_click>"Bark!"</button>
+    }
+}
