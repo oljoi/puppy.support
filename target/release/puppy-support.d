b55aafb04f83545a827387b23f171f8cc7f83689
oljoi <46825856+oljoi@users.noreply.github.com>
2025-11-01 01:24:44 +0200

    binary 4

---
+++ target/release/puppy-support.d
@@ -0,0 +1,1 @@
+/home/oljoi/projects/puppy-support/target/release/puppy-support: /home/oljoi/projects/puppy-support/src/app.rs /home/oljoi/projects/puppy-support/src/lib.rs /home/oljoi/projects/puppy-support/src/main.rs
